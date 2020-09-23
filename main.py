45bc17092987d9d7576261b469c21d52a2a60e6a
OnlyVerga <lorenzo.vergani.270904@gmail.com>
2020-09-23 22:28:33 +0200

    main

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import pygame
 import sys
 import os
+import data.engine as e
+
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, 0) #     set position of the window to right corner of the screen
-
-
 pygame.init()
 
-window = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
 w = 800
 h = 600
+window = pygame.display.set_mode((w, h), pygame.RESIZABLE)
+
+e.load_animations("data/graphics/")
+player = e.entity(0, 0, 16, 16, "player")
+player.set_action("idle")
+player.scale_size(10)
 
 while True:
     for e in pygame.event.get():
@@ -17,7 +22,8 @@ while True:
             sys.exit()
         if e.type == pygame.VIDEORESIZE:
             window = pygame.display.set_mode((e.w, e.h), pygame.RESIZABLE)
-            
+
+    player.display(window, [0, 0])
     w, h = window.get_size()
     pygame.display.update()
     pygame.display.set_caption(str(w) + "x" + str(h))
